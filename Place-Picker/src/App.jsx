320b600f7b77711ed7802ab0f58296a8638fa8b8
krisngohil13 <krisngohil@gmail.com>
2025-01-23 15:21:33 +0400

    New Commit

--- Place-Picker/src/App.jsx
+++ Place-Picker/src/App.jsx
@@ -1,15 +1,38 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+import { use } from 'react';
+
+
+const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const places = storedIDs.map(id => AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
+
+ 
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(places);
+
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
+
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,13 +51,22 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if(storedIDs.indexOf(id) === -1){
+      localStorage.setItem('selectedPlaces', JSON.stringify([id,...storedIDs]));
+    }
   }
+
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIDs.filter(id => id !== selectedPlace.current)));
   }
 
   return (
@@ -63,7 +95,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText={'No places found'}
           onSelectPlace={handleSelectPlace}
         />
       </main>
